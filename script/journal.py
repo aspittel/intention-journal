7a42eaf7091fccb242610e19c53108749878fcb0
Ali Spittel <ali07cat07@gmail.com>
2018-08-13 22:36:36 -0400

    rename vars

--- script/journal.py
+++ script/journal.py
@@ -2,20 +2,20 @@ import datetime, os
 
 import os
 
-def write_header_with_bullet_points(flie, header_text):
-    file.write("\n## " + header_text + "\n")
+def write_header_with_bullet_points(markdown_file, header_text):
+    markdown_file.write("\n## " + header_text + "\n")
     for i in range(3):
-        file.write("* \n")
+        markdown_file.write("* \n")
 
 date = datetime.datetime.today().strftime('%Y-%m-%d')
 current_path = os.path.dirname(os.path.realpath(__file__))
 path = os.path.join(current_path, "../morning/") + date + ".md"
 
-file = open(path, "w+")
+markdown_file = open(path, "w+")
 
-file.write("# " + date + " Morning Journal\n")
+markdown_file.write("# " + date + " Morning Journal\n")
 
-write_header_with_bullet_points(file, "Today I'm grateful for:")
-write_header_with_bullet_points(file, "Today's focus is:")
+write_header_with_bullet_points(markdown_file, "Today I'm grateful for:")
+write_header_with_bullet_points(markdown_file, "Today's focus is:")
 
-file.close()
+markdown_file.close()

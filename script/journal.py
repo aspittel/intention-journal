e58ddc49397ff6873dfefb23789e254580ebc237
Ali Spittel <ali07cat07@gmail.com>
2018-08-14 18:32:21 -0400

    name == main

--- script/journal.py
+++ script/journal.py
@@ -6,18 +6,19 @@ def write_header_with_bullet_points(markdown_file, header_text):
     for i in range(3):
         markdown_file.write("* \n")
 
-date = datetime.datetime.today().strftime('%Y-%m-%d')
-current_path = os.path.dirname(os.path.realpath(__file__))
-morning_path = os.path.join(current_path, "../morning/")
-path = morning_path + date + ".md"
+if __name__ == "__main__":
+    date = datetime.datetime.today().strftime('%Y-%m-%d')
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    morning_path = os.path.join(current_path, "../morning/")
+    path = morning_path + date + ".md"
 
-markdown_file = open(path, "w+")
+    markdown_file = open(path, "w+")
 
-markdown_file.write("# " + date + " Morning Journal\n")
+    markdown_file.write("# " + date + " Morning Journal\n")
 
-write_header_with_bullet_points(markdown_file, "Today I'm grateful for:")
-write_header_with_bullet_points(markdown_file, "Today's focus is:")
+    write_header_with_bullet_points(markdown_file, "Today I'm grateful for:")
+    write_header_with_bullet_points(markdown_file, "Today's focus is:")
 
-markdown_file.close()
+    markdown_file.close()
 
-call(["code", morning_path])
+    call(["code", morning_path])
